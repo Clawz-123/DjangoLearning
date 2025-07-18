50852129d55f43b695d4adfd713c7a784f6f0bc8
Clawz-123 <pukarbohara9@gmail.com>
2025-07-18 12:41:38 +0545

    updating the path and index page

--- pools/urls.py
+++ pools/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "pools"
+ 
 urlpatterns = [path("", views.index, name="index"),
                path("<int:question_id>/", views.detail, name="detail"),
                path("<int:question_id>/results", views.results, name="results"),
