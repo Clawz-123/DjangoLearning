63d603ebd3f392359763769e22617940d476c1e4
Clawz-123 <pukarbohara9@gmail.com>
2025-07-18 11:23:07 +0545

    Implement basic views, templates, and URL routing for polls app

--- pools/views.py
+++ pools/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 # from django.template import loader
-from django.shortcuts import render
+from django.http import Http404
 
 
 from .models import Question
@@ -14,10 +14,15 @@ def index(request):
     context = {"latest_question_list": latest_question_list}
     # output = ",".join([q.question_text for q in latest_question_list])
     # return HttpResponse(context, template)
-    return HttpResponse(request, "pools/index.html", context)
+    return render(request, "pools/index.html", context)
 
 def detail(request, question_id):
-    return HttpResponse("You are looking at question %s."% question_id )
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "pools/details.html", {"question": question})
+    # return HttpResponse("You are looking at question %s."% question_id )
 
 def results(request, question_id):
     response = "Your're looking at the result of question %s"
